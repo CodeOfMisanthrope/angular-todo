d2a1ebd1f1f22008f765c18c2fd644c1295abbd3
Ilya <iosdsbb@gmail.com>
2025-11-06 15:26:06 +0300

    feat: updated

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { TaskDtos } from '../../data/dtos/task';
+import {Component} from '@angular/core';
+import {TaskDtos} from '../../data/dtos/task';
 import {Task} from '../task/task';
 import {UIInput} from '../ui/ui-input/ui-input.component';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UiBtn} from '../ui/ui-btn/ui-btn';
 
 @Component({
   selector: 'app-list',
   imports: [
     Task,
     UIInput,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UiBtn
   ],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
   tasks: TaskDtos[] = [
-    { id: 1, name: 'Изучить Angular' },
-    { id: 2, name: 'Сделать Todo App' },
-    { id: 3, name: 'Пройди собеседование' }
+    {id: 1, name: 'Изучить Angular'},
+    {id: 2, name: 'Сделать Todo App'},
   ];
 
   taskForm: FormGroup;
@@ -29,16 +30,20 @@ export class List {
     });
   }
 
+  // Обработчик для события Submit
   onSubmit() {
-    const taskName = this.taskForm.get('taskName')?.value;
-    console.log(taskName)
+    const taskName = this.taskForm.get('taskName');
+    if (taskName) {
+      const name: string = taskName.value;
+      this.addTask(name);
+    }
   }
 
   // Метод для добавления новой задачи (если нужно)
-  addTask(): void {
+  addTask(name: string): void {
     const newTask: TaskDtos = {
       id: this.tasks.length + 1,
-      name: `Новая задача ${this.tasks.length + 1}`
+      name,
     };
     this.tasks.push(newTask);
   }
