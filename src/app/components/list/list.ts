cb9cc9eb7c1359a4446e5f65f1d8ea54af61092b
Ilya <iosdsbb@gmail.com>
2025-11-06 22:06:12 +0300

    feat: updated list

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -4,29 +4,59 @@ import {Task} from '../task/task';
 import {UIInput} from '../ui/ui-input/ui-input.component';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {UiBtn} from '../ui/ui-btn/ui-btn';
+import {CommonModule} from '@angular/common';
+import {TaskService} from '../../services/task';
 
 @Component({
   selector: 'app-list',
   imports: [
     Task,
     UIInput,
+    UiBtn,
     ReactiveFormsModule,
-    UiBtn
+    CommonModule,
   ],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
-  tasks: TaskDto[] = [
-    {id: 1, name: 'Изучить Angular', isDone: false},
-    {id: 2, name: 'Сделать Todo App', isDone: false},
-  ];
+  // tasks: TaskDto[] = [
+  //   {id: 1, name: 'Изучить Angular', isDone: false},
+  //   {id: 2, name: 'Сделать Todo App', isDone: false},
+  // ];
 
+  tasks: TaskDto[] = [];
   taskForm: FormGroup;
+  isLoading = false;
+  error: string | null = null;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private taskService: TaskService
+  ) {
     this.taskForm = this.fb.group({
       taskName: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.isLoading = true;
+    this.error = null;
+
+    this.taskService.getTasks().subscribe({
+      next: (tasks) => {
+        this.tasks = tasks;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Ошибка загрузки задач:', err);
+        this.error = 'Не удалось загрузить задачи';
+        this.isLoading = false;
+      }
     });
   }
 
