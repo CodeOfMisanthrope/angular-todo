7dfa753e41d52e0772a6503e76bb7b5b961d6dfd
Ilya <iosdsbb@gmail.com>
2025-11-06 15:26:00 +0300

    feat: added ui-btn

---
+++ src/app/components/ui/ui-btn/ui-btn.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-ui-btn',
+  imports: [],
+  templateUrl: './ui-btn.html',
+  styleUrl: './ui-btn.scss',
+})
+export class UiBtn {
+  // @Input() text!: string;
+}
